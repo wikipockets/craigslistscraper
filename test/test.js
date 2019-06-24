017a7955c8cf9d050c9734e749a79249a0f4ad67
Madison <madisonwmoore@gmail.com>
2019-06-23 21:39:50 -0400

    mocha tests

--- test/test.js
+++ test/test.js
@@ -6,4 +6,23 @@ describe("searchItems", function() {
   it("should return a promise", function() {
     expect(app.searchItems("sailboat", "orlando")).to.be.a('promise');
   });
+
+  it("promise should be an array of objects", function() {
+      return app.searchItems("sailboat","orlando").then(function(data){
+        expect(data).to.be.an('array');
+        expect(data[0]).to.be.an("object");
+      })
+  });
+
+  it("objects in array should have property title, price, link",function(){
+        return app.searchItems("sailboat","orlando").then(function(data){
+            expect(data[0]).to.have.property("title");
+            expect(data[0]).to.have.property("price");
+            expect(data[0]).to.have.property("link");
+        })
+  })
+
+
+
+
 });
